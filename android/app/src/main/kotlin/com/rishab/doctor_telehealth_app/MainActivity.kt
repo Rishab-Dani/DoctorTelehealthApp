2433d58e287224d1f747321becea03a5183a71eb
Rishab-Dani <rishabdanii@gmail.com>
2026-07-11 00:16:06 +0530

    Initial Flutter project setup

---
+++ android/app/src/main/kotlin/com/rishab/doctor_telehealth_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rishab.doctor_telehealth_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DoctorApp());
+}
+
+class DoctorApp extends StatelessWidget {
+  const DoctorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Doctor TeleHealth',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Doctor TeleHealth App',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
